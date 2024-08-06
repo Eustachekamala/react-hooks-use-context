668df80ccbf9c2536e0ac4f77d90deda8e7f962f
eustachekamala <eustachejared25@gmail.com>
2024-08-06 15:22:47 +0300

    In this project i used the useContext that allow us to share global data to the components

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,19 @@
-import React from "react";
+import UserContext from "../context/user";
+import React, { useContext } from "react";
 import Interests from "./Interests";
 
-function Profile({ user, theme }) {
+function Profile({ theme }) {
+
+  const { user }= useContext(UserContext);
+
+  function UserProvider({ children }) {
+    const currentUser = {
+      name: 'Duane',
+      interests: ['Coding', 'Biking', "Words ending in 'ing'"],
+    };
+    return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>;
+  }
+  
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
